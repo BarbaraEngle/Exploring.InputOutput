b632553a425dad2f778a2353347d12f379495fb5
barbara.engle <barbara.engle@outlook.com>
2022-09-23 13:15:56 +0200

    Add project files.

---
+++ InputOutput/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace InputOutput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input and Output - System.IO Namespace\n"+
+                              "---------------------------------------");
+
+
+        }
+    }
+}
